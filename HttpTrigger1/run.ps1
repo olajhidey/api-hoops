d7ae7c2df28b57d2af7d24b4ed2f7a729ed9e1c7
olicodema <servicesoliviaa@gmail.com>
2022-01-27 13:24:53 +0100

    Made few changes on the code

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -12,7 +12,7 @@ if (-not $name) {
     $name = $Request.Body.Name
 }
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+$body = "Welcome to HTTP trigger without the name Parameter"
 
 if ($name) {
     $body = "Hello, $name. This HTTP triggered function executed successfully."
